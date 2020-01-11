aad506eb36292f9dc2ce6dd572dae37e0122bc50
karrad2001 <murthy.karra@gmail.com>
2020-01-11 11:46:41 -0800

    Added Congrats components and test

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,11 @@
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value
+ * @function findByTestAttr
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {String} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+
 export const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test]=${val}`);
+  return wrapper.find(`[data-test="${val}"]`);
 };
