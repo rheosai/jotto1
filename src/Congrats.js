43306f05d7683a2fcd9ee5109145c75ff7e7dd8f
karrad2001 <murthy.karra@gmail.com>
2020-01-11 10:04:17 -0800

    First Test

---
+++ src/Congrats.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+/**
+ * Functional react component for Congratulatory message
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop)
+ */
+
+
+
+
+export default () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
